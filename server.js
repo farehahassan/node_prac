7f98fffb44de8f9b6458d9461b49eaa878c7920c
fareha hassan <hfariha104@gmail.com>
2025-02-24 19:35:41 +0500

    .

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 3000;
 // });
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use(require("./middleware/errorHandler"));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
